3dbd489d04e7540389715866b0b6a177c278097c
i.hulidov <i.hulidov@andersenlab.com>
2023-03-26 19:48:26 +0300

    Add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const allPokemonsUrl = 'https://pokeapi.co/api/v2/pokemon?limit=2000';
+export const pokemonDetailsBaseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+export const searchCharactersRequired = 2;
+
+export const routes = {
+  home: '/',
+  details: 'details/:pokemonName',
+};
