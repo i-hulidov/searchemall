ada0dd9c788b8a17faeaa5818866e185399ab6c0
i.hulidov <i.hulidov@andersenlab.com>
2023-03-28 17:17:40 +0300

    Move pokemon image url to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 export const allPokemonsUrl = 'https://pokeapi.co/api/v2/pokemon?limit=2000';
 export const pokemonDetailsBaseUrl = 'https://pokeapi.co/api/v2/pokemon';
+export const pokemonImageBaseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
 
 export const routes = {
   home: '/',
