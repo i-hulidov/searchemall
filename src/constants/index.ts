4ef4c09ea0f0f41286af07c34d151baeb0b5bfc9
i.hulidov <i.hulidov@andersenlab.com>
2023-03-28 11:35:25 +0300

    Remove slash in pokemon details url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const allPokemonsUrl = 'https://pokeapi.co/api/v2/pokemon?limit=2000';
-export const pokemonDetailsBaseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+export const pokemonDetailsBaseUrl = 'https://pokeapi.co/api/v2/pokemon';
 
 export const searchCharactersRequired = 2;
 
