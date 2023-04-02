6b40e320dce9a9e28ac6b47a3852cc0e7fd507e3
i.hulidov <i.hulidov@andersenlab.com>
2023-04-02 19:56:52 +0300

    Add Typescript typings;

--- src/types/Pokemons.ts
+++ src/types/Pokemons.ts
@@ -16,8 +16,42 @@ export type TPokemonType = {
   };
 };
 
+export type TPokemonAbility = {
+  ability: {
+    name: string;
+    url: string;
+  };
+  slot: number;
+  is_hidden: boolean;
+};
+
+type TAbilityFlavorTextEntry = {
+  flavor_text: string;
+  language: {
+    name: string;
+  };
+};
+
+export type TPokemonAbilityDetails = {
+  name: string;
+  flavor_text_entries: TAbilityFlavorTextEntry[];
+};
+
+export type THeldItem = {
+  item: {
+    name: string;
+    url: string;
+  };
+};
+
 export type TPokemonDetails = {
   name: string;
+  order: number;
+  base_experience: number;
+  height: number;
+  weight: number;
+  held_items: THeldItem[];
+  abilities: TPokemonAbility[];
   sprites: {
     front_default: string;
   };
