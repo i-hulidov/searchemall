0eb05b0f628513d656d61868820dc9b41b6015cf
i.hulidov <i.hulidov@andersenlab.com>
2023-04-03 16:01:27 +0300

    Search functionality tests;

--- src/tests/Search.test.tsx
+++ src/tests/Search.test.tsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { act } from 'react-dom/test-utils';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from 'src/App';
+import { Search } from 'src/components/screens';
 import { SearchProvider } from 'src/components/shared/SearchProvider';
 import { SearchContext } from 'src/contexts/SearchContext';
+import { wait } from 'src/utils/wait';
 
 const CustomTest = () => {
   const { searchQuery, setSearchQuery } = useContext(SearchContext);
@@ -18,6 +21,19 @@ const CustomTest = () => {
 };
 
 describe('Search functionality', () => {
+  const originalOffsetHeight = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetHeight');
+  const originalOffsetWidth = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetWidth');
+
+  beforeAll(() => {
+    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', { configurable: true, value: 50 });
+    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', { configurable: true, value: 50 });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', originalOffsetHeight as any);
+    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', originalOffsetWidth as any);
+  });
+
   it('input', () => {
     render(
       <SearchProvider>
@@ -31,5 +47,64 @@ describe('Search functionality', () => {
     fireEvent.change(input, { target: { value: 'Pikachu' } });
 
     expect(element).toHaveTextContent('Pikachu');
+  });
+
+  it('Pokemon is found by full name', async () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <SearchProvider>
+          <Search />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+
+    const input = screen.getByTestId('input');
+
+    await act(async () => {
+      fireEvent.change(input, { target: { value: 'Bulbasaur' } });
+      await wait(2000);
+    });
+
+    const i = getByTestId('bulbasaur');
+    expect(i).toBeTruthy();
+  });
+
+  it('Pokemon is found by containing search string', async () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <SearchProvider>
+          <Search />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+
+    const input = screen.getByTestId('input');
+
+    await act(async () => {
+      fireEvent.change(input, { target: { value: 'saur' } });
+      await wait(2000);
+    });
+
+    const i = getByTestId('bulbasaur');
+    expect(i).toBeTruthy();
+  });
+
+  it('Pokemon is not found', async () => {
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <SearchProvider>
+          <Search />
+        </SearchProvider>
+      </BrowserRouter>,
+    );
+
+    const input = screen.getByTestId('input');
+
+    await act(async () => {
+      fireEvent.change(input, { target: { value: 'Bulbasaur123' } });
+      await wait(2000);
+    });
+
+    expect(getByTestId('not-found')).toBeTruthy();
   });
 });
